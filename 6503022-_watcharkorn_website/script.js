38092330bb91a9f79aa3107729f3ab697bb845f0
Watcharakorn <144198724+ironmanwin1@users.noreply.github.com>
2025-03-29 16:31:40 +0700

    Delete 6503022-_watcharkorn_website directory

--- 6503022-_watcharkorn_website/script.js
+++
@@ -1,8 +0,0 @@
-function updateClock() {
-  const now = new Date();
-  const clock = document.getElementById("clock");
-  clock.textContent = now.toLocaleTimeString();
-}
-
-setInterval(updateClock, 1000);
-updateClock();
